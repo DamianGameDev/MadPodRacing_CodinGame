d047a979feb0fb1bcbce6e4d4281ebd425bb92a3
DamianGameDev <nunez.sanchez.damian@gmail.com>
2021-11-19 23:55:55 -0500

    Promoted to Silver League
    - Got promoted to silver league
    - Organized some code and renamed some functions and variables
    - Commented classes and functions
    
    Note: tried to use DirectionUnitVector to improve strife but it didn't work out as intended

--- MadPodRacingWolcenTest.cpp
+++ MadPodRacingWolcenTest.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+/**
+* Base Actor class which represents a point in space with a radius
+*/
 class BaseActor
 {
 protected:
@@ -16,29 +19,36 @@ public:
 	int GetPositionX() const { return Position_x; }
 	int GetPositionY() const { return Position_y; }
 	int GetRadius() const { return Radius; }
-	void SetPositionX(const int NewPosition) { Position_x = NewPosition; }
-	void SetPositionY(const int NewPosition) { Position_y = NewPosition; }
+	virtual void SetPositionX(const int NewPosition) { Position_x = NewPosition; }
+	virtual void SetPositionY(const int NewPosition) { Position_y = NewPosition; }
 };
 
+/**
+* Class that represents a Checkpoint
+*/
 class CheckPoint : public BaseActor
 {
 protected:
-	int Distance;
+	int Distance; // Distance from previous checkpoint
 
 public:
 	int GetDistance() const { return Distance; }
 	void SetDistance(const int NewDistance) { Distance = NewDistance; }
 };
 
+/**
+* Class manager which stores known checkpoints and when a lap is completed
+*/
 class CheckPointManager
 {
-	vector<CheckPoint*> Checkpoints;
-	int SavedLocation[2] = {};
-	int LongestDistance;
-	u_int8_t bCompletedLap : 1;
-	u_int8_t bAlreadyInitialized : 1;
+	vector<CheckPoint*> Checkpoints; // Vector storing the known checkpoints
+	int SavedLocation[2] = {}; // Last location where a checkpoint was found
+	int LongestDistance; // Longest distance from one checkpoint to another
+	u_int8_t bCompletedLap : 1; // true if a lap was completed(once checkpoint got repeated), false otherwise
+	u_int8_t bAlreadyInitialized : 1; // true if the class was already initialized, false otherwise
 
 public:
+	/** Called to Initialize FirstLocation */
 	void InitializeManager(const int CheckPointPos_x, const int CheckPointPos_y)
 	{
 		if (!bAlreadyInitialized)
@@ -49,6 +59,7 @@ public:
 		}
 	}
 
+	/** Called to Register a found Checkpoint */
 	void RegisterCheckPoint(CheckPoint* NewCheckPoint)
 	{
 		if (!bCompletedLap)
@@ -57,7 +68,7 @@ public:
 			{
 				if (TestCheckPoint->GetPositionX() == NewCheckPoint->GetPositionX() && TestCheckPoint->GetPositionY() == NewCheckPoint->GetPositionY())
 				{
-					cerr << "CompletedLap:" << endl;
+					cerr << "CompletedLap:" << endl; // TODO: Erase debugging code on final
 					bCompletedLap = true;
 					break;
 				}
@@ -74,10 +85,11 @@ public:
 		}
 	}
 
+	/** Returns true if the distance to the next checkpoint is the longest on the lap */
 	bool ShouldBoost(CheckPoint* NextCheckPoint, const int Angle) const
 	{
-		cerr << "LongestDistance:" << LongestDistance << endl;
-		cerr << "CheckPointDistance:" << NextCheckPoint->GetDistance() << endl;
+		cerr << "LongestDistance:" << LongestDistance << endl; // TODO: Erase debugging code on final
+		cerr << "CheckPointDistance:" << NextCheckPoint->GetDistance() << endl; // TODO: Erase debugging code on final
 		if (bCompletedLap)
 		{
 			if (NextCheckPoint->GetDistance() >= LongestDistance && Angle <= 15 && Angle >= -15)
@@ -85,10 +97,10 @@ public:
 				return true;
 			}
 		}
-
 		return false;
 	}
 
+	/** Returns the distance to the next checkpoint */
 	float GetDistanceToNextCheckPoint(const int PosA[2], const int PosB[2])
 	{
 		const int Difference_x = PosA[0] - PosB[0];
@@ -98,7 +110,8 @@ public:
 		return Result;
 	}
 
-	void UpdateCheckpointCounter(const int CheckPointPos_x, const int CheckPointPos_y, const int CheckPointDistance, CheckPointManager* InManager, CheckPoint*& OutCheckPoint)
+	/** Called to update and initialize a checkpoint if not known */
+	void UpdateFoundCheckPoint(const int CheckPointPos_x, const int CheckPointPos_y, const int CheckPointDistance, CheckPointManager* InManager, CheckPoint*& OutCheckPoint)
 	{
 		if (SavedLocation[0] != CheckPointPos_x || SavedLocation[1] != CheckPointPos_y)
 		{
@@ -117,50 +130,73 @@ public:
 	}
 };
 
+/**
+* Class that represent a Pod in the game
+*/
 class Pod : public BaseActor
 {
 protected:
 	float Thrust;
 	float AngleToTarget;
 	float DistanceToTarget;
+	int Speed_x;
+	int Speed_y;
 
 public:
 	int GetThrust() const { return Thrust; }
+	int GetSpeedX() const { return Speed_x; }
+	int GetSpeedY() const { return Speed_y; }
 	void SetAngleToTarget(const int NewAngle) { AngleToTarget = NewAngle; }
 	void SetDistanceToTarget(const int NewDistance) { DistanceToTarget = NewDistance; }
 
+	/** Constructor */
 	Pod()
 	{
 		Radius = 400;
 	}
 
+	/** Called to set the Pod's thrust with some criteria */
 	void SetThrust()
 	{
-		float ThrustMultiplierProximity = DistanceToTarget / (2 * 600); // Double the Checkpoint radius
-		cerr << "ClampedThrustMultiplierProximity:" << ThrustMultiplierProximity << endl;
+		float ThrustMultiplierProximity = DistanceToTarget / (2 * 600); // start decreasing thrust when distance is less than Twice the checkpoint size
+		cerr << "ClampedThrustMultiplierProximity:" << ThrustMultiplierProximity << endl;  // TODO: Erase debugging code on final
 		if (ThrustMultiplierProximity > 1.0f)
 		{
 			ThrustMultiplierProximity = 1.0f;
 		}
-		if (AngleToTarget <= 1 && AngleToTarget >= -1)
+		if (AngleToTarget <= 5 && AngleToTarget >= -5)
 		{
 			Thrust = 100 * ThrustMultiplierProximity;
 		}
 		else
 		{
-			const float NormalizedAngle = AngleToTarget / 90.0f;
-			float ThrustMultiplierAngle = NormalizedAngle < 0 ? 1.0f + NormalizedAngle : 1.0f - NormalizedAngle;
-			if (ThrustMultiplierAngle < 0.0f)
+			const float AngleCeiling = AngleToTarget / 90.0f; // start decreasing thrust when Angle is less than 90° (or higher than -90°)
+			float ThrustMultiplierAngle = AngleCeiling < 0 ? 1.0f + AngleCeiling : 1.0f - AngleCeiling;
+			if (ThrustMultiplierAngle < 0.1f)
 			{
-				ThrustMultiplierAngle = 0.0f;
+				ThrustMultiplierAngle = 0.1f; // Angle limit should not make thrust get under 10%
 			}
-			else if (NormalizedAngle > 1.0f)
+			else if (AngleCeiling > 1.0f)
 			{
 				ThrustMultiplierAngle = 1.0f;
 			}
-			cerr << "ClampedThrustMultiplierAngle:" << ThrustMultiplierAngle << endl;
+			cerr << "ClampedThrustMultiplierAngle:" << ThrustMultiplierAngle << endl; // TODO: Erase debugging code on final
 			Thrust = 100 * ThrustMultiplierAngle * ThrustMultiplierProximity;
 		}
+	}
+
+	/** Called to set position and velocity in X */
+	virtual void SetPositionX(const int NewPosition) override
+	{
+		Speed_x = NewPosition - Position_x;
+		Position_x = NewPosition;
+	}
+
+	/** Called to set position and velocity in Y */
+	virtual void SetPositionY(const int NewPosition) override
+	{
+		Speed_y = NewPosition - Position_y;
+		Position_y = NewPosition;
 	}
 };
 
@@ -174,6 +210,7 @@ int main()
 	CheckPointManager* MyCheckPointManager = new CheckPointManager();
 	CheckPoint* CurrentCheckpoint = new CheckPoint();
 	Pod* MyPod = new Pod();
+	int FrameCounter = 0;
 	// game loop
 	while (1) {
 		int x;
@@ -197,23 +234,35 @@ int main()
 		MyCheckPointManager->InitializeManager(next_checkpoint_x, next_checkpoint_y);
 		MyPod->SetAngleToTarget(next_checkpoint_angle);
 		MyPod->SetDistanceToTarget(next_checkpoint_dist);
+		MyPod->SetPositionX(x);
+		MyPod->SetPositionY(y);
 		MyPod->SetThrust();
 		int CheckPointRadius = CurrentCheckpoint->GetRadius();
 		int thrust = MyPod->GetThrust();
-
-		MyCheckPointManager->UpdateCheckpointCounter(next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, MyCheckPointManager, CurrentCheckpoint);
-
-		if (MyCheckPointManager->ShouldBoost(CurrentCheckpoint, next_checkpoint_angle))
+		int OffsetMultiplier = 3;
+		int TargetOffset_x = FrameCounter > 1 ? MyPod->GetSpeedX() * OffsetMultiplier : 0;
+		int TargetOffset_y = FrameCounter > 1 ? MyPod->GetSpeedY() * OffsetMultiplier : 0;
+		while (FrameCounter <= 1) // This was hardcoded since position on first two frames gave a weird speed (high value)
 		{
-			cout << next_checkpoint_x << " " << next_checkpoint_y << " " << "BOOST" << endl;
-			cerr << "BOOSTING!" << endl;
+			FrameCounter++;
+		}
+
+		cerr << "Speed_x:" << MyPod->GetSpeedX() << endl << "speed_y:" << MyPod->GetSpeedY() << endl; // TODO: Erase debugging code on final
+		cerr << "TargetOffset_x:" << TargetOffset_x << endl << "TargetOffset_y:" << TargetOffset_y << endl; // TODO: Erase debugging code on final
+		MyCheckPointManager->UpdateFoundCheckPoint(next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, MyCheckPointManager, CurrentCheckpoint);
+
+		if (MyCheckPointManager->ShouldBoost(CurrentCheckpoint, next_checkpoint_angle) || (next_checkpoint_dist >= 10000 && next_checkpoint_angle <= 5 && next_checkpoint_angle >= -5))
+		{
+			cout << next_checkpoint_x - TargetOffset_x << " " << next_checkpoint_y - TargetOffset_y << " " << "BOOST" << endl;
+			cerr << "BOOSTING!" << endl; // TODO: Erase debugging code on final
 		}
 		else
 		{
-			cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
+			cout << next_checkpoint_x - TargetOffset_x << " " << next_checkpoint_y - TargetOffset_y << " " << thrust << endl;
 		}
-		cerr << "Distance:" << next_checkpoint_dist << endl << "Angle:" << next_checkpoint_angle << endl << "Thrust:" << thrust << endl;
+		cerr << "Distance:" << next_checkpoint_dist << endl << "Angle:" << next_checkpoint_angle << endl << "Thrust:" << thrust << endl; // TODO: Erase debugging code on final      
 	}
+	// Free space on gameloop end
 	delete MyCheckPointManager;
 	delete CurrentCheckpoint;
 	delete MyPod;
